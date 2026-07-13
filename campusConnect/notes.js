e8d665d16f96b6de06b3c656061e8ac132a8ccf5
khushi Kumari <kk5462464@gmail.com>
2026-07-13 21:18:51 +0530

    connect notes frontend to render backend

--- campusConnect/notes.js
+++ campusConnect/notes.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://campusconnect-dwrq.onrender.com";
 
 const notesForm = document.getElementById("notesForm");
 const notesTable = document.querySelector("#notesTable tbody");

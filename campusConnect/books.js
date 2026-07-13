ff03f340db4517304062ea7ded2ab199b07e007c
khushi Kumari <kk5462464@gmail.com>
2026-07-13 21:37:58 +0530

    connect books frontend to render backend

--- campusConnect/books.js
+++ campusConnect/books.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://campusconnect-dwrq.onrender.com";
 
 const bookForm = document.getElementById("bookForm");
 const booksContainer = document.getElementById("booksContainer");
